7704a9e367edb3a76fcc2dbb536eb334bb3f54eb
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-18 17:53:22 -0700

    Head Function complete

--- assertEqual.js
+++ assertEqual.js
@@ -2,8 +2,10 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`😻 Assertion Passed: ${actual} === ${expected}`);
+    console.log("---");
   } else {
     console.log(`🤮 Assertion Failed: ${actual} !== ${expected}`);
+    console.log("---");
   }
 };
 
