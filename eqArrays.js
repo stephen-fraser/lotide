b1b6fe11e48f00ee11c02896fa76bdc5cae70a98
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-19 18:34:46 -0700

    initial commit of working eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+// Copied over from asserEquals
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log("---");
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log("---");
+  }
+};
+
+const eqArrays = function(array1, array2) {
+
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+      for (let i = 0; i < array1.length; i++) {
+        if (array1[i] !== array2[i]) {
+          return false;
+      } 
+    }
+    return true;
+  }
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3]));
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true);
