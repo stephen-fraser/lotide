f5f51976fb10c47c55fd664a05afd647cbadd6aa
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-20 15:54:41 -0700

    initial push countLetters

---
+++ countLetters.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+    console.log("---");
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+    console.log("---");
+  }
+};
+
+const countLetters = function(letters) {
+  letterCount = {};
+
+  for (let letter of letters) {
+    if (letter === " ") {
+      continue;
+    } else if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      letterCount[letter] = 1;
+    }
+  }
+  return letterCount;
+};
+
+console.log(countLetters('lighthouse in the house'));
