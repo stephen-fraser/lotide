1d78b6e746463791316506a7433f372208512a3a
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-20 17:56:18 -0700

    replaced for in loop with Object.keys and for of

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,25 +8,38 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+// const findKeyByValue = function(object, value) {
+
+//   let objectKey = undefined;
+
+//   for (let x in object) {
+//     if (object[x] === value) {
+//       objectKey = x;
+//     }
+//   }
+//   return objectKey;
+// };
+
 const findKeyByValue = function(object, value) {
 
-  let objectKey = undefined;
+  let keys = Object.keys(object);
+  let results = undefined;
 
-  for (let x in object) {
+  for (let x of keys) {
+  
     if (object[x] === value) {
-      objectKey = x;
+      results = x;
+      break;
     }
   }
-  return objectKey;
+  return results;
 };
-
 
 const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
 };
-
 
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
