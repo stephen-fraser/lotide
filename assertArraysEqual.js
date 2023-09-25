efc4787984cef028d17c82c823016afae2767369
stephen-fraser <steve.james.fraser@gmail.com>
2023-09-25 13:12:38 -0700

    refactored assertEqualsArray / eqArrays with module tests

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,18 +1,4 @@
-const eqArrays = function(array1, array2) {
-
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-      for (let i = 0; i < array1.length; i++) {
-        if (array1[i] !== array2[i]) {
-          return false;
-      } 
-    }
-    return true;
-  }
-};
-
-
+const eqArrays = require("./eqArrays")
 
 const assertArraysEqual = function(actual, expected) {
 
@@ -25,9 +11,3 @@ const assertArraysEqual = function(actual, expected) {
 };
 
 module.exports = assertArraysEqual;
-
-//Tests
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [3, 2, 1]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
